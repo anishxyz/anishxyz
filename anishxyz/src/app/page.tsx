5394a30793702c08a5243c179720c906282523a8
anishagrawal <anishagrawal2003@gmail.com>
2025-01-19 03:53:20 -0500

    beautiful

--- anishxyz/src/app/page.tsx
+++ anishxyz/src/app/page.tsx
@@ -1,9 +1,10 @@
 import ProfileSimple from "@/components/ProfileSimple";
+import Profile from "@/components/Profile"
 
 export default function Home() {
   return (
-    <main className="h-full w-full p-4">
-      <ProfileSimple/>
+    <main className="h-full w-full p-4 flex items-center justify-center md:pt-32">
+        <Profile/>
     </main>
   );
 }
